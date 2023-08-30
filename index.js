f83c8daea26c8d88dc3a0f35276f895649f42218
Max Stoumen <max@mxs.dev>
2023-08-29 21:16:38 -0700

    log

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 import * as artifact from "@actions/artifact";
 
+console.log({
+  token: process.env.ACTIONS_RUNTIME_TOKEN,
+  url: process.env.ACTIONS_RUNTIME_URL,
+});
+
 artifact
   .create()
   .uploadArtifact("my-artifact", ["index.js"], ".")
