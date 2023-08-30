0b3d119e33b16266fa79387c830019353e84eb38
Max Stoumen <max@mxs.dev>
2023-08-29 23:29:57 -0700

    update

--- index.js
+++ index.js
@@ -3,11 +3,12 @@ import * as artifact from "@actions/artifact";
 console.log({
   token: process.env.ACTIONS_RUNTIME_TOKEN,
   url: process.env.ACTIONS_RUNTIME_URL,
+  gh_env: process.env.GITHUB_ENV,
 });
 
-artifact
-  .create()
-  .uploadArtifact("my-artifact", ["index.js"], ".")
-  .then(() => {
-    console.log("Artifact uploaded!");
-  });
+// artifact
+//   .create()
+//   .uploadArtifact("my-artifact", ["index.js"], ".")
+//   .then(() => {
+//     console.log("Artifact uploaded!");
+//   });
