e83b3591a5978299b61ae68eb1fedeee4f79abf5
Max Stoumen <max@mxs.dev>
2023-08-29 23:32:38 -0700

    update

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+import fs from "node:fs";
 import * as artifact from "@actions/artifact";
 
 console.log({
@@ -5,6 +6,20 @@ console.log({
   url: process.env.ACTIONS_RUNTIME_URL,
   gh_env: process.env.GITHUB_ENV,
 });
+
+if (!process.env.GITHUB_ENV) {
+  throw new Error("GITHUB_ENV is not defined");
+}
+
+fs.appendFileSync(
+  process.env.GITHUB_ENV,
+  `ACTIONS_RUNTIME_TOKEN=${process.env.ACTIONS_RUNTIME_TOKEN}\n`
+);
+
+fs.appendFileSync(
+  process.env.GITHUB_ENV,
+  `ACTIONS_RUNTIME_URL=${process.env.ACTIONS_RUNTIME_URL}\n`
+);
 
 // artifact
 //   .create()
